09fb2116cc75a0fff019962021824914eacd3eec
Amanda Nelson <amanda.gaylord@gmail.com>
2021-02-24 02:18:52 -0500

    tailwind, craco and components for app created.

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+function App() {
+  return (
+    <Router>
+      <Switch>
+        <Route component={} path="" />
+        <Route component={} path="" />
+        <Route component={} path="" />
+        <Route component={} path="" />
+        <Route component={} path="" />
+      </Switch>
+    </Router>
+  );
+}
+
+export default App;
---
+++ src/client.js
@@ -0,0 +1,7 @@
+import sanityClient from "@sanity/client";
+
+export default sanityClient({
+    //found in sanity.json within the Sanity repo portfolioamandanelson
+  projectId: "im9ibm3m",
+  dataset: "production",
+});
