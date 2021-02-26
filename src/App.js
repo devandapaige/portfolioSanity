6dad4565dd24d829c303c8c137599bd4641b434f
Amanda Nelson <amanda.gaylord@gmail.com>
2021-02-26 18:11:57 -0500

    components set up

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Home from "./components/Home"
+import About from "./components/About"
+import SinglePost from "./components/SinglePost"
+import Post from "./components/Post"
+import Project from "./components/Project"
+
 function App() {
   return (
     <Router>
       <Switch>
-        <Route component={} path="" />
-        <Route component={} path="" />
-        <Route component={} path="" />
-        <Route component={} path="" />
-        <Route component={} path="" />
+        <Route component={Home} path='' />
+        <Route component={About} path='/about' />
+        <Route component={SinglePost} path='/post/:slug' />
+        <Route component={Post} path="/post" />
+        <Route component={Project} path="/project" />
       </Switch>
     </Router>
   );
---
+++ src/components/SinglePost.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function SinglePost() {
+  return (
+    <>
+      <h1>This is a SinglePost</h1>
+    </>
+  );
+}
